56709724693b59c24d469a985d399f6bf696b21c
Rob de Laat <rob.de.laat@gmail.com>
2020-02-22 19:06:20 +0100

    Added wrapper

--- pdview.py
+++ pdview.py
@@ -9,12 +9,12 @@ from PyQt5.QtCore import Qt
 
 
 class pdvWindow(QWidget):
-    def __init__(self, max_rows=100, max_cols=100, row_width=25, col_width=75):
+    def __init__(self):
         super().__init__()
-        self.row_width = row_width
-        self.col_width = col_width
-        self.max_rows = max_rows
-        self.max_cols = max_cols
+        self.row_height = 25
+        self.col_width = 100
+        self.max_rows = 500
+        self.max_cols = 100 
         self.fmt_default = "{0}"
         self.fmt_int = "{:.0f}"
         self.fmt_float = "{:.5f}"
@@ -44,8 +44,6 @@ class pdvWindow(QWidget):
         self.tbview.horizontalHeader().setHighlightSections(True)
         self.tbview.verticalHeader().setHighlightSections(True)
 
-        self.show()
-
     def set_int_fmt(fmt):
         fmt.format(123)  # Throws if this can't format an int
         self.fmt_int = fmt
@@ -54,18 +52,30 @@ class pdvWindow(QWidget):
         fmt.format(np.pi)  # Throw if this can't format a float
         self.fmt_float = fmt
 
+    def set_max_rows(self, nrows):
+        self.max_rows = nrows
+
+    def set_max_cols(self, ncols):
+        self.max_cols = ncols
+
+    def set_row_height(self, row_height):
+        self.row_height = row_height
+
+    def set_col_width(self, col_width):
+        self.col_width = col_width
+
     def load_df(self, df):
         nrows = df.shape[0] if df.shape[0] < self.max_rows else self.max_rows
         ncols = df.shape[1] if df.shape[1] < self.max_cols else self.max_cols
         self.tbview.setRowCount(nrows)
-        self.tbview.setColumnCount(nrows)
+        self.tbview.setColumnCount(ncols)
         self.tbview.setHorizontalHeaderLabels(df.columns[:nrows].astype('str'))
         self.tbview.setVerticalHeaderLabels(df.index[:ncols].astype('str'))
-        
+
         for icol in range(ncols):
             self.tbview.setColumnWidth(icol, self.col_width)
         for irow in range(nrows):
-            self.tbview.setRowHeight(irow, self.row_width)
+            self.tbview.setRowHeight(irow, self.row_height)
 
 
 
@@ -73,28 +83,54 @@ class pdvWindow(QWidget):
             for icol in range(ncols):
                 val = df.iloc[irow, icol]
 
-                if val.dtype in [np.int]:
-                    fmt_str = self.fmt_int
-                elif val.dtype in [np.float]:
-                    fmt_str = self.fmt_float
-                else:
-                    fmt_str = self.fmt_de
+                fmt_str = self.fmt_default
+
+                if hasattr(val, 'dtype'):
+                    if val.dtype in [np.int]:
+                        fmt_str = self.fmt_int
+                    elif val.dtype in [np.float]:
+                        fmt_str = self.fmt_float
 
                 self.tbview.setItem(irow, icol,
                                     QTableWidgetItem(fmt_str.format(val)))
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = pdvWindow()
+class PDView():
+    def __init__(self, df):
+        self.app = QApplication([])
+        self.ui = pdvWindow()
+        self.ui.load_df(df)
 
-    m = 10000
+    def set_int_fmt(self, fmt):
+        self.ui.set_int_fmt(fmt)
+
+    def set_float_fmt(self, float):
+        self.ui.set_float_fmt(fmt)
+
+    def set_max_rows(self, nrows):
+        self.ui.set_max_rows(nrows)
+
+    def set_max_cols(self, ncols):
+        self.ui.set_max_cols(ncols)
+
+    def set_row_height(self, row_height):
+        self.ui.set_row_height(row_height)
+
+    def set_col_width(self, col_width):
+        self.ui.set_col_width(col_width)
+
+    def show(self):
+        self.ui.show()
+        sys.exit(self.app.exec_())
+
+
+if __name__ == '__main__':
+    m = 100
     n = 100
     df = pd.DataFrame(
         np.random.rand(m, n),
         index=np.arange(0, m),
         columns=['col_' + str(x) for x in range(n)]
         )
-
-    ex.load_df(df)
-    sys.exit(app.exec_())
+    p = PDView(df)
+    p.show()
